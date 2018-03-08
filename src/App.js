f575ee9a5b30d380d79beb64af33e83204b4d24d
David Goodwin <goodwid@gmail.com>
2018-03-08 13:53:31 -0800

    Working display quotes with a component

--- src/App.js
+++ src/App.js
@@ -2,18 +2,44 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      quotes: []
+    };
+  }
+  componentDidMount() {
+    fetch('http://localhost:9000/api/quotedb/v1/quotes')
+      .then(results => results.json())
+      .then(data => {
+        let quotes = data;
+        this.setState({quotes});
+      });
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <p>Quotes are here: </p>
+          {this.state.quotes.map(item => <DisplayQuote key={item._id} data={item}/>)}
+        </div>
       </div>
     );
   }
 }
 
+function DisplayQuote(props) {
+  return <div>
+    {props.data.data.split('\n').map((el, key) => <span key={key}>{el}<br/></span>)}
+  </div>
+}
+
 export default App;
+
+// return <li key={item._id}>
+//               { 
+//                 item.data
+//                 .split('\n')
+//                 .map(el => <span>{el}<br/></span>)
+//               }
+//             </li>
